812afff1e4396ca3bb1b04d80c6f82ad7faaf90b
hemantDwivedi <hemantdwiwedi@gmail.com>
2023-04-06 13:01:32 +0530

    added Role Model and Repo

---
+++ src/main/java/com/inverse/project/Jobless/models/Role.java
@@ -0,0 +1,19 @@
+package com.inverse.project.Jobless.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "role")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+    @Id
+    private int id;
+    private String name;
+}
